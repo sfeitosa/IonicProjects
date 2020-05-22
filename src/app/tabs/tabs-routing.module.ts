97c28e082650459c5e8a489b240fd0859d597940
Samuel Feitosa <samuel.feitosa@ifsc.edu.br>
2020-05-22 16:58:01 -0300

    First commit.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,13 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
+
+      {
+        path: 'feed',
+        loadChildren: () => import('../feed/feed.module').then(
+                              m => m.FeedPageModule)
+      },
+
       {
         path: '',
         redirectTo: '/tabs/tab1',
